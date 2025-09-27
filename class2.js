6715092dcdfac4e5f38a93999cbe7e79ee32c821
impressivecoder1 <rohanrahman251@gmail.com>
2025-09-28 02:49:24 +0600

    class2

---
+++ class2.js
@@ -0,0 +1,17 @@
+//introduction to while loop  
+const number = 7;
+if(number < 1){
+    console.log('choto number')
+}
+console.log('none')
+
+let num = 3; //loop variable
+// while(num < 5){
+//     console.log('looop')
+// }
+// console.log('n')
+while(num < 11){
+    console.log('looping', num)
+    num += 1;
+    // num++
+}
