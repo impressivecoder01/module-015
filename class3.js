e9d6e1b4a13ddc44f8d0ef08d8c6eee42d50a237
impressivecoder1 <rohanrahman251@gmail.com>
2025-09-29 01:18:19 +0600

    class3.js

--- class3.js
+++ class3.js
@@ -1,6 +1,25 @@
 //problem solving with while loop
-let num = 0
-while(num < 10){
-    num += 1
-    console.log(num)
+// let num = 0
+// while(num < 10){
+//     console.log(num)
+//     num ++
+// }
+//-------------- sum 1-10
+// let number = 1;
+// let sum = 0;
+// while(number<=10){
+//     console.log(number)
+//     sum = sum + number;
+//     number ++
+//     console.log('total', sum)
+// }
+
+//----------------------- even  number 
+let num = 1;
+while(num <= 10){
+    // console.log(num)
+    if(num % 2 === 0){
+        console.log('even number', num)
+    }
+    num++
 }
