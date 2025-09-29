6030e55bb2e352c48a2b3175c83bd945763860a7
impressivecoder1 <rohanrahman251@gmail.com>
2025-09-30 01:31:45 +0600

    class5done

--- class5.js
+++ class5.js
@@ -1,9 +1,21 @@
 //problem solving with For loop
 
 //--------sum
-let sum = 0;
-for(let i = 0; i < 10; i ++){
-    // console.log(i)
-    sum = sum + i;
+// let sum = 0;
+// for(let i = 0; i < 10; i ++){
+//     // console.log(i)
+//     sum = sum + i;
+// }
+// console.log(sum)
+
+//------- decremental for loop
+// for(let i = 10; i >= 0 ; i--){
+//     console.log(i)
+// }
+
+//------------while loop decremental
+let i = 15
+while(i >= 0){
+    i--;
+    console.log(i)
 }
-console.log(sum)
