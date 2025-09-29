4db2f036c51e0ba4272cd671d7658b2bb75bdb35
impressivecoder1 <rohanrahman251@gmail.com>
2025-09-29 23:35:15 +0600

    class4added

---
+++ class4.js
@@ -0,0 +1,28 @@
+//introduction to For loop
+// let num = 0;
+// while(num < 5){
+//     num++;
+//     console.log(num)
+// }
+
+// for loop stature
+// for(first part; second part; third part){
+// loop statement
+// }
+// 1. loop variable declaration; 
+// 2.loop condition; 
+// 3. loop variable change
+
+// for (let num = 0; num < 7; num++){
+//     console.log(num)
+// }
+
+// ------------ even number finding
+for(let num = 0; num <= 10; num= num + 2){
+    console.log(num)
+}
+
+// ------------ odd number finding
+for(let num = 1; num <= 20; num = num + 2){
+    console.log(num)
+}
