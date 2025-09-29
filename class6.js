773bedda1a07f12123f99b4c6d6121b2b1dd632a
impressivecoder1 <rohanrahman251@gmail.com>
2025-09-30 02:07:42 +0600

    class6donebyme

---
+++ class6.js
@@ -0,0 +1,20 @@
+//Different ways to use loop
+for (let i = 0; i < 20; i++){
+    if(i%2===0){
+        console.log(i)
+    }
+}
+
+for(let i = 0; i < 25; i = i+ 2){
+    console.log(i)
+}
+
+for(let i = 0; i < 30; i++){
+    if(i % 2 !==0){
+        console.log(i)
+    }
+}
+
+for(let i = 1; i < 34; i = i + 2){
+    console.log(i)
+}
